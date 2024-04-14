eb321b328d31b72ef5ae3ea1dc9eabc39b13224b
Fabio <fabioescobarardila@hotmail.com>
2024-04-14 14:51:17 -0500

    Implementando rutas principales y rutas hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import { HeaderComponent } from './shared/header/header.component';
     BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
   ],
   imports: [
     BrowserModule,
