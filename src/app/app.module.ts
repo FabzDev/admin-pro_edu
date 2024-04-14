22ef00374c5121a2cf8cf8a4bea7a193e38a9100
Fabio <fabioescobarardila@hotmail.com>
2024-04-14 13:19:23 -0500

    Implementando header, breadcrumbs, sidebat y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { Page404Component } from './pages/page404/page404.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     DashboardComponent,
     BreadcrumbsComponent,
     SidebarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
